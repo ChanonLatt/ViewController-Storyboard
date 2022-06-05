a88f4dc67ce5c0ec77b8e0270a10f9793552af87
Latt Chanon <68848964+ChanonLatt@users.noreply.github.com>
2022-06-05 12:32:05 +0700

    Add files via upload

---
+++ ViewController + Storyboard.xctemplate/XIB filegenerateXIBfile/___FILEBASENAME___.swift
@@ -0,0 +1,46 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: Z1BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        commitUI()
+        prepareComponentObservers()
+    }
+    
+}
+
+// MARK: - Computed Properties
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+
+}
+
+// MARK: -Public Functions
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    
+}
+
+// MARK: -Private Functions
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    private func commitUI() {
+        
+    }
+    
+    private func prepareComponentObservers() {
+        
+    }
+}
+
+//MARK: - Ojective C Functions
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    
+}
+
+
+
